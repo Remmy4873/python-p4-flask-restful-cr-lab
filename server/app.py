9eddd78c9c5d80a3bcdab37f9484d34761e37b9d
Remmy4873 <Remmykiplangat4873@gmail.com>
2024-10-07 23:49:12 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -17,11 +17,29 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plant_list = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(plant_list, 200)
+        return response 
+
+    def post(self):
+        new_plant = Plant(name=request.json['name'], price=request.json['price'], 
+                          image=request.json['image'])
+        db.session.add(new_plant)
+        db.session.commit()
+        new_plant_dict = new_plant.to_dict()
+        response = make_response(new_plant_dict, 200)
+        return response
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        response = make_response(plant, 200)
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')  
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
